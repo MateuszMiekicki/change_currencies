e02b76e6fe80ed946bf2cf307b5f007888735faf
Mateusz Miękicki <miekickimateusz@gmail.com>
2021-03-10 13:36:16 +0100

    Added a parsing class definition for the national Polish bank.

---
+++ include/parser/nbp/ParserNBP.hpp
@@ -0,0 +1,22 @@
+#ifndef ParserNBP_hpp
+#define ParserNBP_hpp
+
+#include "../exchangeRate/Rate.hpp"
+#include "../exchangeRate/currency/Currency.hpp"
+#include "parser/Parser.hpp"
+#include <string>
+#include <unordered_map>
+
+class ParserNBP : public Parser
+{
+private:
+    std::unordered_map<Currency, Rate> _data;
+
+public:
+    virtual std::unordered_map<Currency,
+                               Rate>
+    parse(const std::string &argument) override;
+    virtual std::unordered_map<Currency, Rate> getResult() const override;
+};
+
+#endif
