dc2b854e7594c93e088268968b6d28d2c78aa3f6
Mateusz Miękicki <miekickimateusz@gmail.com>
2021-03-26 15:38:11 +0100

    Adding a field in the interface to parse API responses with currency exchange.
    Addition of a field responsible for the effective date of the course.

--- include/parser/Parser.hpp
+++ include/parser/Parser.hpp
@@ -3,6 +3,7 @@
 
 #include "../exchangeRate/Rate.hpp"
 #include "../exchangeRate/currency/Currency.hpp"
+#include "date/date.h"
 #include <string>
 #include <unordered_map>
 
@@ -12,6 +13,7 @@ public:
     virtual std::unordered_map<Currency,
                                Rate>
     parse(const std::string &argument) = 0;
+    virtual date::year_month_day getEffectiveDate() const = 0;
     virtual std::unordered_map<Currency, Rate> getResult() const = 0;
     virtual ~Parser(){};
 };
